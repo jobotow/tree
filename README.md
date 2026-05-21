dd0536adb712375cfa8c9c578aa2578911d41134
jobotow <jbotchwayowusu@gmail.com>
2026-05-21 12:58:23 +0100

    Made tree a package(library)

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # tree
-Output in tree format
+A simple, zero dependency go library for rendering tree structures on the terminal.
+
+## Installation
+```bash
+go get github.com/jobotow/tree
+```
+
+## Usage
+```go
+package main
+import (
+    "fmt"
+    "github.com/jobotow/tree"
+)
+
+func main(){
+    data := []tree.TreeNode{
+        {Text: "Root", Children: []tree.TreeNode{
+            {Text: "Branch 1", Children:
+            []tree.TreeNode{
+                {Text: "Leaf 1"},
+                {Text: "Leaf 2"},
+            }},
+            {Text: "Branch 2", Children:
+            []tree.TreeNode{
+                {Text: "Leaf 3"},
+            }},
+        }},
+    }
+	fmt.Println(tree.Tree(data))
+}
+```
+
+## Output 
+```bash
+Root
+├── Branch 1
+│   ├── Leaf 1
+│   └── Leaf 2
+└── Branch 2
+    └── Leaf 3
+```
--- tree.go
+++ tree.go
@@ -1,17 +1,14 @@
-package main
-import (
-	"fmt"
+package tree
 
-)
+import "fmt"
+
 
 type TreeNode struct{
 	Text string
 	Children []TreeNode
 }
 
-func Tree(items []TreeNode, prefix string, parentIsLast, isRoot bool) string {
-	//prefix = ""
-	//parentIsLast =
+func renderTree(items []TreeNode, prefix string, parentIsLast, isRoot bool) string {
 	var line, result, childPrefix string
 	result = ""
 	for i, node := range items {
@@ -23,16 +20,15 @@ func Tree(items []TreeNode, prefix string, parentIsLast, isRoot bool) string {
 			switch isLastItem{
 			case true:
 				line = fmt.Sprintf("%s└── %s\n", prefix, node.Text)
-				childPrefix = "    "
+				childPrefix = fmt.Sprintf("%s    ", prefix)
 			case false:
 				line = fmt.Sprintf("%s├── %s\n", prefix, node.Text)
-				childPrefix = "│   "
+				childPrefix = fmt.Sprintf("%s│   ", prefix)
 			}
 		}
 		result += line
 		if len(node.Children) > 0 {
-			fmt.Printf("Child PREFIX: '%s'\n", childPrefix)
-			result += Tree(node.Children, childPrefix, isLastItem, false)
+			result += renderTree(node.Children, childPrefix, isLastItem, false)
 		}
 		if isLastItem && isRoot && len(node.Children) == 0 {
 			result += fmt.Sprintf("%s\n", prefix)
@@ -41,53 +37,7 @@ func Tree(items []TreeNode, prefix string, parentIsLast, isRoot bool) string {
 	return result
 }
 
-func main(){
-	myTree := []TreeNode{
-		{Text: "Level 1", Children:
-	[]TreeNode{
-		{Text: "Level 2", Children:
-	[]TreeNode{
-		{Text: "Level 3", Children:
-	[]TreeNode{
-		{Text: "Level 4"}, // Children:
-	//[]TreeNode{
-	//	{Text: "Level 5"},
-	//}},
-	}},
-	}},
-	}},
-	}
-	fmt.Println(Tree(myTree, "", true, true))
-	
+
+func Tree(items []TreeNode) string {
+	return renderTree(items, "", true, true)
 }
-
-
-/*
-    def self.render_tree(items, prefix = "", parent_is_last=true, is_root=true)
-      result = ""
-
-      items.each_with_index do |node, i|
-        is_last_item = (i == items.length - 1)
-
-        line = if is_root
-          "#{node[:text]}\n"
-        else
-          "#{HEITT::Color.colorize(prefix, :blue)}#{HEITT::Color.colorize((is_last_item ? '└── ' : '├── '), :blue)}#{node[:text]}\n"
-        end
-
-        child_prefix = if is_root
-          ""
-        else
-          "#{HEITT::Color.colorize(prefix, :bold, :blue)}#{HEITT::Color.colorize((is_last_item ? "    " : "│   "), :bold, :blue)}"
-        end
-        result += line 
-        result += render_tree(node[:children], child_prefix, is_last_item, false) 
-		if node[:children].any?
-      
-        if is_last_item && !is_root and !node[:children].any?
-          result += "#{HEITT::Color.colorize(prefix, :bold, :blue)}  \n"
-        end
-      end
-      result
-    end
-/*/
